0d55efb0b788a1f4039dcbfc5bbf4e4109b46263
Almir Merzic <almir.merzic@finshark.io>
2025-04-06 02:15:04 +0200

    Added node js express api project setup, with contact form api's.

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const newsletterRoutes = require('./routes/newsletter');
+const contactRoutes = require('./routes/contact');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// Use external routes
+app.use('/newsletter', newsletterRoutes);
+app.use('/contact', contactRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
